706bc9c51a4d5e9995eb623e9b47544772a4f04d
kltoken <wkle_123@126.com>
2018-01-26 17:10:04 +0800

    finish

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-function(){
-	$('#div').mouseDown(function(){
 
+
+$(function(){
+	$('#div').mousedown(function(ev){
+		var _this = $(this);
+		var disX = ev.pageX - _this.offset().left;
+		var disY = ev.pageY - _this.offset().top;
+
+		var leftLimit = $(window).width() - _this.width();
+		var topLimit = $(window).height() - _this.height();
+
+		$(document).mousemove(function(ev){
+			var leftVal = ev.pageX-disX<0 ? 0 : 
+				( ev.pageX-disX > leftLimit ) ? leftLimit : ev.pageX-disX;
+
+			var topVal = ev.pageY-disY<0 ? 0 :
+				( ev.pageY-disY > topLimit ) ? topLimit : ev.pageY-disY;
+			// console.dir(window)
+			_this.offset({left:leftVal,top:topVal});
+
+			$(document).mouseup(function(ev){
+				$(document).off('mousemove');
+				return false;
+			})
+		})
 	})
-}
+})
